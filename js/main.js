ec435fcdf9baa7a15834eb5dc24a0ecbf9579722
Mark Nowowiejski <marknowowiejski@CBS-MNOWOWI1-M.local>
2018-11-28 19:46:18 -0500

    fix js/css linking issue

--- js/main.js
+++ js/main.js
@@ -1,20 +1,7 @@
-var cards = document.querySelectorAll("div.wiley-flip-card");
+var cards = document.querySelectorAll("div.flashcard");
 
 for (var i = 0; i < cards.length; i++) {
 	cards[i].addEventListener("click", function() {
-    this.className = (this.className === 'wiley-flip-card') ? 'wiley-flip-card flipped' : 'wiley-flip-card';
+    this.className = (this.className === 'flashcard') ? 'flashcard flipped' : 'flashcard';
   });
-}
-
-var cssId = 'flipcards';
-
-if (!document.getElementById(cssId)) {
-  var head  = document.getElementsByTagName('head')[0];
-  var link  = document.createElement('link');
-  link.id   = cssId;
-  link.rel  = 'stylesheet';
-  link.type = 'text/css';
-  link.href = 'https://content2.learntoday.info/media_team_tools/flipcard/public/css/cards.css';
-  link.media = 'all';
-  head.appendChild(link);
 }
