cac5d1a6ed45f510f74a55a908a5d636df28c729
Mark Nowowiejski <marknowowiejski@CBS-MNOWOWI1-M.local>
2018-12-04 12:14:41 -0500

    add LMS integration scripts

--- js/main.js
+++ js/main.js
@@ -5,3 +5,16 @@ for (var i = 0; i < cards.length; i++) {
     this.className = (this.className === 'flashcard') ? 'flashcard flipped' : 'flashcard';
   });
 }
+
+// var cssId = 'flipcards';
+//
+// if (!document.getElementById(cssId)) {
+//   var head  = document.getElementsByTagName('head')[0];
+//   var link  = document.createElement('link');
+//   link.id   = cssId;
+//   link.rel  = 'stylesheet';
+//   link.type = 'text/css';
+//   link.href = 'https://content2.learntoday.info/media_team_tools/flipcard/public/css/cards.css';
+//   link.media = 'all';
+//   head.appendChild(link);
+// }
---
+++ js/old.js
@@ -0,0 +1,1 @@
+<script type="text/javascript" src="https://content2.learntoday.info/media_team_tools/flipcard/public/js/cards.js"></script>
